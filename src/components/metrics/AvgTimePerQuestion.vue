930b5445fd278227d3b8399cdbde0dc8377cecb5
crajas@cs.washington.edu <chandnir02@gmail.com>
2022-03-23 14:54:07 -0700

    added base filtering functionality

--- src/components/metrics/AvgTimePerQuestion.vue
+++ src/components/metrics/AvgTimePerQuestion.vue
@@ -1,11 +1,19 @@
 <template>
-  <select id="graph-filter">
-    <option value="lessThan">Less Than</option>
-    <option value="greaterThan">Greater Than</option>
-  </select>
-
-  <input id="filterLimit" placeholder="Filter Limit" /> <br /><br />
-  <button class="button" onClick="applyFilter(filterLimit)">Apply Filter</button>
+<form @change="updateGraph">
+  <h1>Filtering: </h1>
+  <label>
+    <input type="radio" name="filtering" value="smallest">
+      Smallest 10 Entries
+  </label>
+  <label>
+    <input type="radio" name="filtering" value="largest">
+      Highest 10 Entries
+  </label>
+  <label>
+    <input checked type="radio" name="filtering" value="mix">
+      Smallest 5 and Largest 5 Entries
+  </label>
+  </form>
   <BarChart :data=this.data :stylingLabels=this.styling></BarChart>
 </template>
 
@@ -22,7 +30,8 @@ export default {
   },
   data() {
     return {
-      data: [],
+      data: [[],[]],
+      allData: [[], []],
       styling: {
          text: "Average Time Spent Per Question",
          labelStringX: "Time (seconds)",
@@ -31,9 +40,29 @@ export default {
     };
   },
   created() {
+    this.allData = this.graphData();
     this.data = this.graphData();
   },
   methods: {
+    updateGraph() {
+      console.log(this.allData[0].length);
+      let labels = this.allData[0];
+      let values = this.allData[1]
+      let options = document.querySelector("input:checked").value;
+      let size = values.length;
+      if (options==="smallest") {
+        labels = labels.slice(0, 3);
+        values = values.slice(0, 3);
+      } else if (options==="largest") {
+        labels = labels.slice(values.length - 3, size);
+        values = values.slice(values.length - 3, size);
+      } else {
+        labels = [].concat(labels.slice(0,3), labels.slice(size-3, size));
+        values = [].concat(values.slice(0,3), values.slice(size-3, size));
+      }
+      this.data = [labels, values];
+    },
+
     /**
      * Returns an array of labels for the chart followed by an array of corresponding
      * data points that indicate the height of the bar for a specific label. The labels
@@ -58,6 +87,12 @@ export default {
 
         avgAnswerTimes.push(user["value"].toFixed(2));
       });
+
+      let size = avgAnswerTimes.length;
+      if (size > 5) {
+        questionLabels = [].concat(questionLabels.slice(0,3), questionLabels.slice(size-3, size));
+        avgAnswerTimes = [].concat(avgAnswerTimes.slice(0, 3), avgAnswerTimes.slice(size-3, size));
+      }
 
       return [questionLabels, avgAnswerTimes];
     },
@@ -120,3 +155,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 15px;
+  font-weight: bold;
+  font-style: italic;
+  margin-bottom: 0px;
+  display:inline;
+}
+
+form {
+  margin-top: 50px;
+}
+</style>
